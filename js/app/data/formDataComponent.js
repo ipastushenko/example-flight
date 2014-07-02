18b69f833034bcc871b3f50ce046ad29ce9d90c7
ipastushenko <ivan.pastushenko@7bits.it>
2014-07-02 13:58:21 +0700

    add template form component

---
+++ js/app/data/formDataComponent.js
@@ -0,0 +1,28 @@
+var FormData = flight.component(function () {
+	this.defaultAttrs({
+
+	});
+
+	this.on_submit = function (e, data) {
+		var url = "/" + e.target.url.value + ".json";
+		$.ajax({
+        	type: "GET",
+        	url: url,
+        	dataType: "json"
+	    }).done(function(data, textStatus, jqXHR) {
+	    	if (data.success) {
+	        	alert("Url: " + url + ", Success");
+	        }
+	        else {
+	        	alert("Url: " + url + ", Error: " + data.error);
+	        }
+	    }).fail(function(jqXHR, textStatus, error) {
+	        alert("Url: " + url + ", Error: " + error);
+	    });		
+		return false;
+	}
+
+	this.after("initialize", function() {
+		this.on("submit", this.on_submit)
+	});
+});
---
+++ js/app/main.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+	FormData.attachTo("#form")
+});
