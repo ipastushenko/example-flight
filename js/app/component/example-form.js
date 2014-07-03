3daf0d7cf6add8d455b6d9fb13c0cbc3c4726f63
ipastushenko <ivan.pastushenko@7bits.it>
2014-07-03 16:30:47 +0700

    add simple validate on form

--- js/app/component/example-form.js
+++ js/app/component/example-form.js
@@ -1,9 +1,16 @@
 var ExampleForm = flight.component(Loader, function () {
     this.defaultAttrs({
-        loader: "#loader",
+        //loader
+        loader: "#loader",        
         loaderShowEvent: "showLoader",
         loaderHideEvent: "hideLoader",
+        //validation
+        usernameError: "#username-error",
+        passwordError: "#password-error",        
+        showErrorDelay: 1500,
+        //general
         submitButton: "submit-button",
+        //for emulate errors
         delay: 5000,
         urlSelector: "url-selector",
         successUrl: "/success.json",
@@ -29,14 +36,34 @@ var ExampleForm = flight.component(Loader, function () {
         });
     }
 
+    this.validation = function(form) {
+        var is_error = false;
+        if (!form.username.value) {
+            $(this.attr.usernameError).html("Username can't be blank");
+            $(this.attr.usernameError).show(0).delay(this.attr.showErrorDelay).hide(0);
+            is_error = true;
+        }
+        if (!form.password.value) {
+            $(this.attr.passwordError).html("Password can't be blank");
+            $(this.attr.passwordError).show(0).delay(this.attr.showErrorDelay).hide(0);
+            is_error = true;
+        }
+
+        return !is_error;
+    }
+
     this.on_submit = function (e) {
         var form = e.target;
+        if (!this.validation(form)) {
+            return false;
+        }
+
         var url;
         if (form[this.attr.urlSelector].checked) {
             url = this.attr.errorUrl;
         } else {
             url = this.attr.successUrl;
-        }        
+        }
 
         form[this.attr.submitButton].disabled = true;
         this.trigger(this.attr.loaderShowEvent);
