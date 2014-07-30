fc4ee5993e1a715ab4c32cf9ab0c2d40f4bd7106
ipastushenko <ivan.pastushenko@7bits.it>
2014-07-30 15:43:39 +0700

    Add example table UI mixin

--- js/app/component/example-table.js
+++ js/app/component/example-table.js
@@ -1,1 +1,6 @@
- 
+var exampleTableComponent = flight.component(exampleTableUIMixin, loaderMixin, function() {
+	this.defaultAttrs({
+		//loader
+		loader: "#table-loader"
+	})
+});
---
+++ js/app/mixin/example-table-ui.js
@@ -0,0 +1,3 @@
+var exampleTableUIMixin = function () {
+	
+}
